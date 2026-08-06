2eb7bcc105da3fc911b9e67f04ec3da5f71f9f77
bs21082010 <bs21082010@users.noreply.github.com>
2026-08-06 18:46:10 +0530

    Add APK download site with upload page

---
+++ config.js
@@ -0,0 +1,5 @@
+const APP_URL = "https://iqfmvqdyrajrugghcetb.supabase.co";
+const APP_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlxZm12cWR5cmFqcnVnZ2hjZXRiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUwNDQxODAsImV4cCI6MjEwMDYyMDE4MH0.xfmaddaRioiTM5c7HF6iIvHtqS9PREEotm8F_MbjV9k";
+const BUCKET_NAME = "apks";
+const APP_NAME = "ITC";
+const SITE_URL = "https://iqfmvqdyrajrugghcetb.supabase.co/storage/v1/object/public/" + BUCKET_NAME + "/";
